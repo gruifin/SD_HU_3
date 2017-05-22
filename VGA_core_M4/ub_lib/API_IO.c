9b328bc241857c72ff2b4ae46c7d3b46736baa45
Jasper de Zoete <jasper176@live.nl>
2017-05-22 15:15:06 +0200

    Added 0 width function
    for rectangle and triangle

--- VGA_core_M4/ub_lib/API_IO.c
+++ VGA_core_M4/ub_lib/API_IO.c
@@ -26,10 +26,14 @@ void UB_VGA_Line(uint16_t x1, uint16_t y1,uint16_t x2,uint16_t y2,uint16_t width
 	ry = -1*ry;
 	}
 		for(j = 0; j < d; j++){
+			if(width != 0){
 			for (i=(-1*width); i < width; i++){
 				for (k=(-1*width); k < width; k++){
-				UB_VGA_SetPixel(x1+k*rx+(j*rx),y1+i*ry+(j*ry),color);
+				UB_VGA_SetPixel(x1+k*ry+(j*rx),y1+i*rx+(j*ry),color);
 				}
+			}
+			}else{
+				UB_VGA_SetPixel(x1+(j*rx),y1+(j*ry),color);
 			}
 		}
 }
