58e8e3e020afe5137e6c91b3251577aedd4e037e
ludo teirlinck <ludo.teir@hotmail.com>
2017-06-08 21:44:10 +0200

    update

--- VESOFTON-16/API/html/search/defines_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['array_5fx',['ARRAY_X',['../_a_p_i___draw_8h.html#a4d28f433aea364a3ad46e595fdcf0d5c',1,'API_Draw.h']]],
-  ['array_5fy',['ARRAY_Y',['../_a_p_i___draw_8h.html#a269b7afbb0895a1f631c8381d0eb8f0a',1,'API_Draw.h']]]
-];
--- VESOFTON-16/API/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['api_5fdraw_2eh',['API_Draw.h',['../_a_p_i___draw_8h.html',1,'']]]
-];
